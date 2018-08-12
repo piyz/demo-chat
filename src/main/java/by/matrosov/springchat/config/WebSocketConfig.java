a0d1a5c389e9f27619fcf067b31dc7bc3d18d7b7
Andrey Matrosov <matrosovs2008@yandex.ru>
2018-08-12 19:54:25 +0300

    first commit

---
+++ src/main/java/by/matrosov/springchat/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package by.matrosov.springchat.config;
+
+import by.matrosov.springchat.interceptor.HttpHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final HttpHandshakeInterceptor handshakeInterceptor;
+
+    @Autowired
+    public WebSocketConfig(HttpHandshakeInterceptor handshakeInterceptor) {
+        this.handshakeInterceptor = handshakeInterceptor;
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS().setInterceptors(handshakeInterceptor);
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
